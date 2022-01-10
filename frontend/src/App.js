8746e3009c1a1c7d58d00ba8c9ba63ee0fb3d3cc
LarissaRC <larissa.carvalho0309@gmail.com>
2022-01-10 10:57:58 -0400

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1>Título</h1>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from '../../assets/img/github.svg';
+import './style.css';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>
+                <a href="https://github.com/LarissaRC/">
+                    <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/LarissaRC</p>
+                    </div>
+                </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
